4f8f92e450698326e327220f20512d31c53abd61
ivan augusto <ivan.abt69@outlook.com>
2020-06-01 00:23:02 -0400

    winner picker quasar

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink'
+
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+      
+    }
+  }
+}
+</script>
